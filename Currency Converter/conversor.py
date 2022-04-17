e0d3c2fb70518f4bcfe977af9b2095ed15205d1c
rolandbarreto <rolandbarreto19@gmail.com>
2022-04-17 07:08:11 -0600

    Adding conversor program

---
+++ Currency Converter/conversor.py
@@ -0,0 +1,7 @@
+pesos = input('Cuantos pesos colombianos tienes? ')
+pesos = float(pesos)
+valor_dolar = 3875
+dolares = pesos / valor_dolar
+dolares = str(dolares)
+
+print('Tienes $' + dolares + ' dólares')
