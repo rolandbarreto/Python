f3447af6fe1adefe84ce2d626856342624bb5125
rolandbarreto <rolandbarreto19@gmail.com>
2022-04-17 16:06:47 -0600

    Added round function

--- Currency Converter/conversor.py
+++ Currency Converter/conversor.py
@@ -2,6 +2,7 @@ pesos = input('Cuantos pesos colombianos tienes? ')
 pesos = float(pesos)
 valor_dolar = 3875
 dolares = pesos / valor_dolar
+dolares = round(dolares, 2)
 dolares = str(dolares)
 
 print('Tienes $' + dolares + ' dólares')
