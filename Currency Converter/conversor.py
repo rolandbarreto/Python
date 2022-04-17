be6b03feb0b321129f918bad5ddc759f92f98e83
rolandbarreto <rolandbarreto19@gmail.com>
2022-04-17 16:38:45 -0600

    Added more currencies

--- Currency Converter/conversor.py
+++ Currency Converter/conversor.py
@@ -1,21 +1,59 @@
-pesos = input('Cuantos pesos colombianos tienes? ')
-pesos = float(pesos)
-valor_dolar = 3875
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
+menu = """
+Bienvenidos al conversor de monedas 💵
 
-print('-------------------------------')
-print('Tienes $' + dolares + ' dólares')
-print('-------------------------------\n')
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos
+4 - Cordobas nicaraguenses
 
-cordobas = input('Cuantos cordobas tienes? ')
-cordobas = float(pesos)
-valor_dolar = 36.10
-dolares = cordobas / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
+Elige una opcion: """ 
 
-print('-------------------------------')
-print('Tienes $' + dolares + ' dólares')
-print('-------------------------------')
+opcion = int(input(menu))
+
+if opcion == 1:
+    pesos = input('Cuantos pesos colombianos tienes? ')
+    pesos = float(pesos)
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+
+    print('-------------------------------')
+    print('Tienes $' + dolares + ' dólares')
+    print('-------------------------------\n')
+elif opcion == 2:
+    pesos = input('Cuantos pesos argentinos tienes? ')
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+
+    print('-------------------------------')
+    print('Tienes $' + dolares + ' dólares')
+    print('-------------------------------\n')
+elif opcion == 3:
+    pesos = input('Cuantos pesos mexicanos tienes? ')
+    pesos = float(pesos)
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+
+    print('-------------------------------')
+    print('Tienes $' + dolares + ' dólares')
+    print('-------------------------------\n')
+elif opcion == 4:
+    cordobas = input('Cuantos cordobas tienes? ')
+    cordobas = float(cordobas)
+    valor_dolar = 36.10
+    dolares = cordobas / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+
+    print('-------------------------------')
+    print('Tienes $' + dolares + ' dólares')
+    print('-------------------------------')
+else:
+    print('Ingresa una opcion correcta!')
+
