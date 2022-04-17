d78a776b2be7efa523c08c1a9d788b3bb4ae6d77
rolandbarreto <rolandbarreto19@gmail.com>
2022-04-17 16:12:22 -0600

    Added nicaraguan currency

--- Currency Converter/conversor.py
+++ Currency Converter/conversor.py
@@ -5,4 +5,17 @@ dolares = pesos / valor_dolar
 dolares = round(dolares, 2)
 dolares = str(dolares)
 
+print('-------------------------------')
 print('Tienes $' + dolares + ' dólares')
+print('-------------------------------\n')
+
+cordobas = input('Cuantos cordobas tienes? ')
+cordobas = float(pesos)
+valor_dolar = 36.10
+dolares = cordobas / valor_dolar
+dolares = round(dolares, 2)
+dolares = str(dolares)
+
+print('-------------------------------')
+print('Tienes $' + dolares + ' dólares')
+print('-------------------------------')
